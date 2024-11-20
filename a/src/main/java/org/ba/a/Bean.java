adf12c8d548c852eafdd2c999db3d81e5c4434d3
Andrey <bursov@drucksofa.de>
2024-11-20 10:01:51 +0100

    bean self deploy

--- a/src/main/java/org/ba/a/Bean.java
+++ a/src/main/java/org/ba/a/Bean.java
@@ -1,22 +1,23 @@
-package org.ba.bean;
+package org.ba.a;
 
-import org.ba.commons.Util;
-import javax.ejb.Schedule;
-import javax.ejb.Stateless;
 
-@Stateless
+import jakarta.ejb.Schedule;
+import jakarta.ejb.Singleton;
+
+
+@Singleton
 public class Bean {
+    private static int count = 0;
+    private static long start = System.currentTimeMillis();
 
-    @Schedule(hour = "*", minute = "*", second = "0", persistent = false)
+    @Schedule(hour = "*", minute = "*", second = "*/6", persistent = false)
     private void task() {
-        try {
-        String result = Util.getString();
-        System.out.println(result);
-        } catch (Throwable e) {
-            System.out.println("ERROR, " + e.toString());
-        }
+        long s = (System.currentTimeMillis()-start)/1000;
+
+        String html = JsoupProvider.getString();
+        System.out.printf("%d:%02d, iteration %d, %s\n", s/60, s%60, ++count, html);
     }
-    
+
     public static void main(String[] args) {
         new Bean().task();
     }
---
+++ a/src/main/java/org/ba/a/JsoupProvider.java
@@ -0,0 +1,25 @@
+package org.ba.a;
+
+
+/*import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;*/
+
+public class JsoupProvider {
+
+    public static String getString() {
+
+/*        try {
+            Document doc = Jsoup.connect("http://google.com").get();
+            return doc.location();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }*/
+
+        return null;
+    }
+
+
+}
